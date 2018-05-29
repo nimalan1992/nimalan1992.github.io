45443f90a0d64750254c5e1a44c92139e7d1a5b5
nimalan1992 <39304604+nimalan1992@users.noreply.github.com>
2018-05-29 15:36:25 +0200

    Node js

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("my code is loaded and running");
+setTimeout(function(){
+document.querySelector("hi").style.color="#ff0000";
+},3000);
+}, 1500);
+
+setInterval(function);{
+    document.querySelector("hi").style.color="#00dd00";
+},3000);
