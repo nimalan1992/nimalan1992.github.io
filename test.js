f68c975ec634fb4bdb56de7746eb1942d1686deb
nimalan1992 <39304604+nimalan1992@users.noreply.github.com>
2018-06-26 12:53:30 +0200

    fds

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("my code is loaded and running");
-setTimeout(function(){
-document.querySelector("hi").style.color="#ff0000";
-},3000);
-}, 1500);
-
-setInterval(function);{
-    document.querySelector("hi").style.color="#00dd00";
-},3000);
